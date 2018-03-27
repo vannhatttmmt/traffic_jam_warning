32b04864a9c9834e6dfb86c79b1a58e6a8d022be
VANNHATTTMMT\vannhatttmmt <vannhatttmmt@gmail.com>
2018-03-27 19:17:35 +0700

    oiu

---
+++ src/main/java/application/controller/Controller.java
@@ -0,0 +1,16 @@
+package application.controller;
+
+import application.model.Greeting;
+import application.model.HelloMessage;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+
+@org.springframework.stereotype.Controller
+public class Controller {
+    @MessageMapping("/hello")
+    @SendTo("/topic/greetings")
+    public Greeting greeting(HelloMessage message) throws Exception {
+        Thread.sleep(1000); // simulated delay
+        return new Greeting("Hello, " + message.getName() + "!");
+    }
+}
---
+++ src/main/java/application/model/HelloMessage.java
@@ -0,0 +1,21 @@
+package application.model;
+
+public class HelloMessage {
+
+    private String name;
+
+    public HelloMessage() {
+    }
+
+    public HelloMessage(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
